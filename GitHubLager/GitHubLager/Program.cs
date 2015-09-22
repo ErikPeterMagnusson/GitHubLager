d8f49231d4bd03ccfb08a0b7212cf6a27da885a0
Minatanae <christoffer@mierei.com>
2015-09-22 14:03:14 +0200

    PROGRAM.CS WITH CASES

--- GitHubLager/GitHubLager/Program.cs
+++ GitHubLager/GitHubLager/Program.cs
@@ -26,53 +26,54 @@ namespace GitHubLager
             Console.WriteLine("// Välkommen till GitHubLager");
             Console.WriteLine("// Tryck på X för att visa lista på ordrar:");
             Console.WriteLine("===============================================");
-
-            // SWITCH
             string switch1 = Console.ReadLine();
             Console.WriteLine("===============================================");
+
+            // CREATE LIST
+            List<Item> orderItems = new List<Item>();
+            orderItems.Add(item4);
+            orderItems.Add(item3);
+            orderItems.Add(item2);
+            orderItems.Add(item1);
+
+            // DETAILS FOR ORDER 1
+            var order1 = new Order();
+            order1.Item = "Test produkt";
+            order1.Customer = "Test AB";
+            order1.OrderNumber = 1;
+            order1.OrderDescription = "Nya varor till renovering.";
+            order1.ShippingAdress = "Testgatan 1, 81010 Testa";
+
+            // DETAILS FOR ORDER 2
+            var order2 = new Order();
+            order2.Item = "Koskötare";
+            order2.Customer = "Mejeriet AB";
+            order2.OrderNumber = 2;
+            order2.OrderDescription = "Uppgradera ladan";
+            order2.ShippingAdress = "Kogatan 1, 84420 Koland";
+
+            // DETAILS FOR ORDER 3
+            var order3 = new Order();
+            order3.Item = "Kylplatta";
+            order3.Customer = "Datakompaniet AB";
+            order3.OrderDescription = "Byggmaterial till serverlokal.";
+            order3.OrderNumber = 3;
+            order3.ShippingAdress = "Datagatan 4, 82250 Compland";
+
+            // ADD ORDERS TO LIST
+            var orders = new List<Order>();
+            orders.Add(order1);
+            orders.Add(order2);
+            orders.Add(order3);
+
+            //SWITCH START
             switch (switch1)
-            {
-                // CASE 1: SHOW LIST OF ALL ORDERS
+            {               
+                // CASE X: SHOW LIST OF ALL ORDERS
                 case "X":
                 case "x":
-                    List<Item> orderItems = new List<Item>();
-                    orderItems.Add(item4);
-                    orderItems.Add(item3);
-                    orderItems.Add(item2);
-                    orderItems.Add(item1);
 
-                    // DETAILS FOR ORDER 1
-                    var order1 = new Order();
-                    order1.Item = "Test produkt";
-                    order1.Customer = "Test AB";
-                    order1.OrderNumber = 1;
-                    order1.OrderDescription = "Nya varor till renovering.";
-                    order1.ShippingAdress = "Testgatan 1, 81010 Testa";
-
-                    // DETAILS FOR ORDER 2
-                    var order2 = new Order();
-                    order2.Item = "Koskötare";
-                    order2.Customer = "Mejeriet AB";
-                    order2.OrderNumber = 2;
-                    order2.OrderDescription = "Uppgradera ladan";
-                    order2.ShippingAdress = "Kogatan 1, 84420 Koland";
-
-                    // DETAILS FOR ORDER 3
-                    var order3 = new Order();
-                    order3.Item = "Kylplatta";
-                    order3.Customer = "Datakompaniet AB";
-                    order3.OrderDescription = "Byggmaterial till serverlokal.";
-                    order3.OrderNumber = 3;
-                    order3.ShippingAdress = "Datagatan 4, 82250 Compland";
-
-                    // ADD ORDERS TO LIST
-                    var orders = new List<Order>();
-                    orders.Add(order1);
-                    orders.Add(order2);
-                    orders.Add(order3);
-
-                    // SHOW LIST OF ORDERS
-                    foreach (Order x in orders)
+                foreach (Order x in orders)
                     {
                         Console.WriteLine("Produktnamn: " + x.Item);
                         Console.WriteLine("Kund: " + x.Customer);
@@ -82,10 +83,42 @@ namespace GitHubLager
                         Console.WriteLine("===============================================");
                         
                     }
-                    Console.ReadKey();
-
-                    break;
-            }
-        }
-    }
-}
+                Console.ReadKey();
+                break;
+                // CASE 1: SHOW ORDER 1
+                case "1":
+                var order = orders.Single(x => x.OrderNumber == 1);
+                    Console.WriteLine("Produktnamn: " + order.Item);
+                    Console.WriteLine("Kund: " + order.Customer);
+                    Console.WriteLine("Orderbeskrivning: " + order.OrderDescription);
+                    Console.WriteLine("Ordernummer: " + order.OrderNumber);
+                    Console.WriteLine("Kund: " + order.Customer);
+                    Console.WriteLine("===============================================");
+                Console.ReadKey();
+                break;
+                // CASE 2: SHOW ORDER 2
+                case "2":
+                var order22 = orders.Single(x => x.OrderNumber == 2);
+                Console.WriteLine("Produktnamn: " + order22.Item);
+                Console.WriteLine("Kund: " + order22.Customer);
+                Console.WriteLine("Orderbeskrivning: " + order22.OrderDescription);
+                Console.WriteLine("Ordernummer: " + order22.OrderNumber);
+                Console.WriteLine("Kund: " + order22.Customer);
+                Console.WriteLine("===============================================");
+                Console.ReadKey();
+                break;
+                // CASE 3: SHOW ORDER 3
+                case "3":
+                var order33 = orders.Single(x => x.OrderNumber == 3);
+                Console.WriteLine("Produktnamn: " + order33.Item);
+                Console.WriteLine("Kund: " + order33.Customer);
+                Console.WriteLine("Orderbeskrivning: " + order33.OrderDescription);
+                Console.WriteLine("Ordernummer: " + order33.OrderNumber);
+                Console.WriteLine("Kund: " + order33.Customer);
+                Console.WriteLine("===============================================");
+                Console.ReadKey();
+                break;
+            } // SWITCH END
+        } // MAIN END
+    } //PROGRAM END
+} //NAMESPACE END
