76ff67785ebfa974ad9f194ea4b878d090660269
Aron <rorran4@hotmail.com>
2015-09-22 12:50:28 +0200

    new product in Product.cs

--- GitHubLager/GitHubLager/Program.cs
+++ GitHubLager/GitHubLager/Program.cs
@@ -11,26 +11,35 @@ namespace GitHubLager
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen till GitHubLager");
+            Console.WriteLine("");
             Console.ReadKey();
 
             var order1 = new Order();
-            order1.Item = "Produkt Ett";
+            order1.Item = "Test produkt";
             order1.Customer = "Test AB";
             order1.OrderDescription = "Nya varor till renovering.";
             order1.OrderNumber = 1;
             order1.ShippingAdress = "Testgatan 1, 81010 Testa";
 
             var order2 = new Order();
-            order2.Item = "Produkt Två";
+            order2.Item = "Koskötare";
             order2.Customer = "Mejeriet AB";
             order2.OrderDescription = "Uppgradera ladan";
             order2.OrderNumber = 2;
             order2.ShippingAdress = "Kogatan 1, 84420 Koland";
 
+            var order3 = new Order();
+            order3.Item = "Kylplatta";
+            order3.Customer = "Datakompaniet AB";
+            order3.OrderDescription = "Byggmaterial till serverlokal.";
+            order3.OrderNumber = 3;
+            order3.ShippingAdress = "Datagatan 4, 82250 Compland";
+
 
             var orders = new List<Order>();
             orders.Add(order1);
             orders.Add(order2);
+            orders.Add(order3);
 
 
             foreach (Order x in orders)
