ecac9ef87a7a4f84df9c1b50ecec4e448fdd69ae
Aron <rorran4@hotmail.com>
2015-09-22 11:26:27 +0200

    added code in Program.cs

--- GitHubLager/GitHubLager/Program.cs
+++ GitHubLager/GitHubLager/Program.cs
@@ -12,6 +12,31 @@ namespace GitHubLager
         {
             Console.WriteLine("Välkommen till GitHubLager");
             Console.ReadKey();
+
+            var order1 = new Order();
+            order1.Item = "Produkt Ett";
+            order1.Customer = "Test AB";
+            order1.OrderDescription = "Nya varor till renovering.";
+            order1.OrderNumber = 1;
+            order1.ShippingAdress = "Testgatan 1, 81010 Testa";
+
+            var order2 = new Order();
+            order2.Item = "Produkt Två";
+            order2.Customer = "Mejeriet AB";
+            order2.OrderDescription = "Uppgradera ladan";
+            order2.OrderNumber = 2;
+            order2.ShippingAdress = "Kogatan 1, 84420 Koland";
+
+
+            var orders = new List<Order>();
+            orders.Add(order1);
+            orders.Add(order2);
+
+
+            foreach (Order x in orders)
+            {               
+                Console.WriteLine();
+            }
         }
     }
 }
