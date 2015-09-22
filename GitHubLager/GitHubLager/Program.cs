39cafbdd5aa2d88604952f5006a0f574abda08ae
ErikPeterMagnusson <erikpeterme@gmail.com>
2015-09-22 13:16:31 +0200

    Commit error

--- GitHubLager/GitHubLager/Program.cs
+++ GitHubLager/GitHubLager/Program.cs
@@ -24,11 +24,15 @@ namespace GitHubLager
             item4.Name = "Item 4";
 
             List<Item> orderItems = new List<Item>();
+            orderItems.Add(item4);
+            orderItems.Add(item3);
+            orderItems.Add(item2);
+            orderItems.Add(item1);
 
             var order1 = new Order();
             order1.Item = "Test produkt";
             order1.Customer = "Test AB";
-            order1.OrderItems = "";
+            order1.OrderItems = orderItems;
             order1.OrderNumber = 1;
             order1.OrderDescription = "Nya varor till renovering.";
             order1.ShippingAdress = "Testgatan 1, 81010 Testa";
@@ -36,6 +40,7 @@ namespace GitHubLager
             var order2 = new Order();
             order2.Item = "Koskötare";
             order2.Customer = "Mejeriet AB";
+            order2.OrderItems = orderItems;
             order2.OrderNumber = 2;
             order2.OrderDescription = "Uppgradera ladan";
             order2.ShippingAdress = "Kogatan 1, 84420 Koland";
@@ -43,8 +48,9 @@ namespace GitHubLager
             var order3 = new Order();
             order3.Item = "Kylplatta";
             order3.Customer = "Datakompaniet AB";
-            order3.OrderDescription = "Byggmaterial till serverlokal.";
+            order3.OrderItems = orderItems;
             order3.OrderNumber = 3;
+            order3.OrderDescription = "Byggmaterial till serverlokal.";
             order3.ShippingAdress = "Datagatan 4, 82250 Compland";
 
 
@@ -60,12 +66,15 @@ namespace GitHubLager
                 Console.WriteLine("Kund: " + x.Customer);
                 Console.WriteLine("Orderbeskrivning: " + x.OrderDescription);
                 Console.WriteLine("Ordernummer: " + x.OrderNumber);
-                Console.WriteLine("Vi skickar ordern till adressen: " + x.ShippingAdress);
+                Console.WriteLine("Kund: " + x.Customer);
                 Console.WriteLine("");
                 Console.ReadKey();
             }
-            Console.WriteLine(order1.OrderItems);
+            foreach (Item i in orderItems)
+            {               
+            Console.WriteLine(i);
                 Console.ReadKey();
+            }
         }
     }
 }
